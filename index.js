1de4fea3ebcb5825ad5afa9ef8f98db3eab3b0f8
daninkc <41238045+daninkc@users.noreply.github.com>
2020-06-25 00:32:47 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 
 const apiRouter = require('./routes/api');
+const { Router } = require('express');
 
 require('./db_queries');
 
@@ -11,7 +12,11 @@ app.use(bodyParser.urlencoded({ extended: true}));
 
 app.use('/', apiRouter);
 
+
 app.listen(3000, () => {
     console.log("It's alive!");
 });
 
+app.get("/", (req, res) => {
+    res.send("Oh, you're here! What a pleasant surprise");
+  });
